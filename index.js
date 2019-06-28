486db85780529ff330ded1b36c292d95414cefb9
Sam Roquitte <samroq@yahoo.com>
2019-06-28 12:04:32 -0500

    Figured out how the API actually works

--- index.js
+++ index.js
@@ -8,32 +8,6 @@ const streamLink = "https://www.twitch.tv/overwatchleague";
 
 let childProcess;
 
-function main() {
-  request(requestLink, (err, res, body) => {
-    const matchSchedule = JSON.parse(body);
-    const nextMatch = matchSchedule.data.nextMatch;
-
-    const nextMatchStartDate = new Date(nextMatch.startDate);
-    const nextMatchEndDate = new Date(nextMatch.endDate);
-
-    console.log(
-      chalk.blue(`Next match: ${nextMatchStartDate} - ${nextMatchEndDate}`)
-    );
-
-    if (nextMatchStartDate > nextMatchEndDate) {
-      console.log(chalk.red(Error("Start date is after end date")));
-      process.exit(1);
-    }
-
-    schedule.scheduleJob(nextMatchStartDate, openStream);
-    console.log(chalk.yellow(`Scheduled stream open: ${nextMatchStartDate}`));
-    schedule.scheduleJob(nextMatchEndDate, () => childProcess.kill());
-
-    // Wait 20 min to wait for api to update match
-    setTimeout(main, 1200000);
-  });
-}
-
 function openStream() {
   childProcess = spawn("firefox", [streamLink]);
   childProcess.stdout.on("data", data => {
@@ -44,25 +18,36 @@ function openStream() {
   });
 }
 
-// Check if match is live now
-request(requestLink, (err, res, body) => {
-  const matchSchedule = JSON.parse(body);
-  const now = new Date();
-  const liveMatch = matchSchedule.data.liveMatch;
-  if (!liveMatch) {
-    main();
-  }
-  const liveMatchStartDate = new Date(liveMatch.startDate);
-  const liveMatchEndDate = new Date(liveMatch.endDate);
-  if (now > liveMatchStartDate && now < liveMatchEndDate) {
-    // Match is live now!!
-    console.log(chalk.yellow("Match live now!"));
-    openStream();
-    schedule.scheduleJob(liveMatchEndDate, () => childProcess.kill());
-    setTimeout(main, 1200000);
-  } else {
-    // No live match, schedule for next match
-    console.log(chalk.yellow("No live match now :("));
-    main();
-  }
-});
+function closeStream() {
+  childProcess.kill();
+  main();
+}
+
+function main() {
+  request(requestLink, (err, res, body) => {
+    const matchSchedule = JSON.parse(body);
+    const liveMatch = matchSchedule.data.liveMatch;
+    const liveMatchStartDate = new Date(liveMatch.startDate);
+    const liveMatchEndDate = new Date(liveMatch.endDate);
+    if (liveMatch.liveStatus == "LIVE") {
+      console.log(chalk.yellow("Match live now!"));
+      openStream();
+      schedule.scheduleJob(liveMatchEndDate, closeStream);
+      console.log(
+        chalk.blue(`Scheduled stream close for: ${chalk.cyan(liveMatchEndDate)}`)
+      );
+    } else {
+      console.log(chalk.yellow("No live match now :("));
+      schedule.scheduleJob(liveMatchStartDate, openStream);
+      schedule.scheduleJob(liveMatchEndDate, closeStream);
+      console.log(
+        chalk.blue(`Scheduled stream open for: ${chalk.cyan(liveMatchStartDate)}`)
+      );
+      console.log(
+        chalk.blue(`Scheduled stream close for: ${chalk.cyan(liveMatchEndDate)}`)
+      );
+    }
+  });
+}
+
+main();
